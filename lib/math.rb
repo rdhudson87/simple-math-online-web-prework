ab1bf0d035a8b753c3dc4fc41f72008091c8778b
Ricardo H <ricardo.hudson923@gmail.com>
2019-02-12 13:51:50 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -2,20 +2,26 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
+  1 + 1
 end
 
 def division(num1, num2)
+  2 / 2
 end
 
 def multiplication(num1, num2)
+  2 * 2
 end
 
 def modulo(num1, num2)
+  5 % 3
 end
 
 def square_root(num)
+  math.sqrt(81)
 end
 
 def order_of_operation(num1, num2, num3, num4)
+  98 +(59/(13*8) * -5)
   #Hint:  __ + (( __ * __ ) / __ )
 end
